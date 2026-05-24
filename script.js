0006943d7c083713d4f509b1e9c9ebdecdb78f30
denilly2 <r1094522@student.thomasmore.be>
2026-05-24 21:38:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const URL = "./model/";
 let model, webcam, maxPredictions;
 
 const aiChoices = ["✋", "✊", "✌️"];
+
+async function init() {
+const modelURL = URL + "model.json";
+const metadataURL = URL + "metadata.json";
+
+model = await tmImage.load(modelURL, metadataURL);
+
+webcam = new tmImage.Webcam(400, 300, true);
+await webcam.setup();
+await webcam.play();
+
+const video = document.getElementById("webcam");
+video.replaceWith(webcam.canvas);
+webcam.canvas.id = "webcam";
+}
